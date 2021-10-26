356c7d35d4e44cd91a4241e607226f882474bc27
Sebas0317 <sebastiansandoval12371@gmail.com>
2021-10-26 12:37:51 -0500

    cambios en el evento del inicio

--- src/Components/view-inicio/eventCards.jsx
+++ src/Components/view-inicio/eventCards.jsx
@@ -6,8 +6,8 @@ class EventCard extends React.Component {
     render(props) {
         return (
             <div>
-                <Card className="cards bg-transparent border-light mt-5 mb-5 rounded-2" style={{ width: '22rem', margin: ' 10px auto' }}>
-                    <Card.Img variant="top" src={this.props.imagen} style={{ width: '130px', height: '100px', marginBottom: '25px', alignItems: 'center', justifyContent: "center"}}/>
+                <Card className="cards bg-transparent  mt-5 mb-5 rounded-2" style={{ width: '300', margin: ' 10px 50px' }}>
+                    <Card.Img variant="top" src={this.props.imagen} style={{width: '130px', height: '130px', margin: '0 auto'}}/>
                     <Card.Body>
                         <Card.Title className="text-center fs-3 text-white fw-bold">{this.props.titulo}</Card.Title>
                         <Card.Text className="text-center fs-5 text-white">
