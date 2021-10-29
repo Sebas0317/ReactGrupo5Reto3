1ef1e324a7a70d172e402e50d2bdd1fe4ce1f203
Veranis Nuñez <Veranisnb@gmail.com>
2021-10-29 00:25:05 -0500

    Ajuste

--- src/Components/view-inicio/Inicio_Container.jsx
+++ src/Components/view-inicio/Inicio_Container.jsx
@@ -10,7 +10,7 @@ import Comentarios from "./All_comments"
 class Inicio_Container extends React.Component{
   render(){
     return(
-      <div className="container-fluid p-0 m-0">
+      <div className="container-fluid p-0 m-0 ">
         <React.Fragment>
           <Carousel />
           <main className="main">
