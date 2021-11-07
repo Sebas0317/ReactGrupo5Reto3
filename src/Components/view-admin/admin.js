3f9ad2cae4652a092b2160f1f73747e8dcaa031d
Moi6AP <moiplay300@gmail.com>
2021-11-07 02:21:13 -0500

    admin.js

--- src/Components/view-admin/admin.js
+++ src/Components/view-admin/admin.js
@@ -1,16 +1,18 @@
 import {useState, useEffect} from "react";
 import {useHistory} from "react-router-dom";
 import Services from "../view-services/Admin_Services";
+import verPass from "../assets/verPass.svg";
+import verPass1 from "../assets/verPassNone.svg";
 import "../styles/admin.css";
 
 export default function Admin (){
 	const history = useHistory();
 
-	let [servicios, setServicios] = useState(true);
+	let [servicios, setServicios] = useState(false);
 	let [menu, setMenu] = useState(false);
 	let [reservas, setReservas] = useState(false);
-	let [usuarios, setUsuarios] = useState(false);
-	let [redi, setRedi] = useState("");
+	let [usuarios, setUsuarios] = useState(true);
+	let [redi, setRedi] = useState("a");
 
 	let session = JSON.parse(localStorage.getItem("session"));
 
@@ -19,6 +21,8 @@ export default function Admin (){
 			history.push("/login");
 		}
 	}
+
+	let users = JSON.parse(localStorage.getItem("users"));
 
 	useEffect(()=>{
 		if (session.user.rol == "admin") {
@@ -78,29 +82,77 @@ export default function Admin (){
 		}
 	});
 
+	let img = [false, false, false, false];
+	function verPassFunct(e){
+		let inputPass = document.querySelector("#inputPassAdmin"+e);
+		let imgVerPass = document.querySelector("#imgVerPass"+e);
+
+		if (inputPass.type == "text") {
+			inputPass.type="password";
+		} else if (inputPass.type == "password"){
+			inputPass.type="text";
+		}
+
+
+		if (img[e] == false) {
+			imgVerPass.src = verPass1;
+			img[e] = true;
+		} else {
+			imgVerPass.src = verPass;
+			img[e] = false;
+		}
+
+	}
+
 	if (session.user.rol == "admin") {
 		return (
 			<div className="adminCont">
 					<div className="navAdmin">
-						<div id="btnServicios" className="opcionAdmin seleccion marginTop"><span>Editar servicios</span></div>
+						<div id="btnUsuarios" className="opcionAdmin seleccion"><span>Editar usuarios</span></div>
+						<div id="btnServicios" className="opcionAdmin marginTop"><span>Editar servicios</span></div>
 						<div id="btnMenu" className="opcionAdmin"><span>Editar menu</span></div>
 						<div id="btnReservas" className="opcionAdmin"><span>Editar reservas</span></div>
-						<div id="btnUsuarios" className="opcionAdmin"><span>Editar usuarios</span></div>
 					</div>
 					<div style={{display:"flex", width:"100%", height:"100vh"}}>
 						{ servicios &&
-							<div><p>Servicios</p></div>
+							<div className="parteAdmin">
+								<input type="button" onClick={()=>history.push("/gestionservicios")} value="Ir a la seccion editar servicios"/>
+							</div>
 						}
 						{ reservas &&
-							<div><p>Reservas</p></div>
+							<div className="parteAdmin">
+							<	p>Reservas</p>
+							</div>
 
 						}
 						{ menu &&
-							<div><p>Menu</p></div>
+							<div className="parteAdmin">
+								<input type="button" onClick={()=>history.push("/gestionmenu")} value="Ir a la seccion editar menu"/>
+							</div>
 
 						}
 						{ usuarios &&
-							<div><p>Usuarios</p></div>
+							<div className="parteAdmin">
+								<div className="casillasAdmin">
+									<div className="contCasilla">
+										<div className="casillaAdmin" id="contCasillaTop">Nombre</div>
+										<div className="casillaAdmin" id="contCasillaTop">Correo</div>
+										<div className="casillaAdmin" id="contCasillaTop">Contraseña</div>
+										<div className="casillaAdmin" id="contCasillaTop">rol</div>
+									</div>
+								{ (users) && users.map((user, index)=>{
+									return (
+										<div className="contCasilla">
+											<div className="casillaAdmin"><p>{user.name}</p></div>
+											<div className="casillaAdmin"><p>{user.user}</p></div>
+											<div className="casillaAdmin"><input value={user.pass} id={"inputPassAdmin"+index} type="password"/><img id={"imgVerPass"+index} onClick={()=>verPassFunct(index)} src={verPass}/></div>
+											<div className="casillaAdmin"><p>{user.rol}</p></div>
+										</div>
+									)
+								})
+								}
+								</div>
+							</div>
 
 						}
 					</div>
