f39e4dd4ad10e6c5778534e2194da045d14dd38a
Moi6AP <moiplay300@gmail.com>
2021-11-19 00:30:37 -0500

    view-carrito

--- src/Components/view-carrito/All_menus.jsx
+++ src/Components/view-carrito/All_menus.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Menu_carrito from "./Menu_carrito";
 import Modal from "../modal/modal";
 
 function All_menus (){
 
   let [modal, setModal] = useState(false);
-  let [number, setNumber] = useState(false);
+  let [obj, setObj] = useState(0);
+  let [number, setNumber] = useState(0);
   let [del, setDel] = useState(false);
   let list = []
-  let val = localStorage.getItem('pedidos')
+  let val = localStorage.getItem('pedidos');
+
   if (val) {
     list = JSON.parse(val);
   }
 
-  function eliminar(e) {
+  function eliminar(id) {
     let list = JSON.parse(localStorage.getItem("pedidos"));
-    let id = list.findIndex(obj => obj.Nombre == e);
     list.splice(id, 1);
     localStorage.setItem("pedidos", JSON.stringify(list));
     document.querySelector(".closeModal").click();
@@ -24,15 +25,61 @@ function All_menus (){
     }
   }
 
+  function abrir(id){
+    setNumber(id);
+    setModal(true);
+  }
+
+  function sumar(id){
+    let plato = JSON.parse(localStorage.getItem("pedidos"));
+    let val = plato[id].Precio;
+    let total = plato[id].Total;
+    let result = plato[id].Total + plato[id].Precio;
+    list[id].Total = result;
+    localStorage.setItem("pedidos", JSON.stringify(list));
+    obj++
+    setObj(obj);   
+  }
+
+  function restar(id){
+    let plato = JSON.parse(localStorage.getItem("pedidos"));
+    if (plato[id].Total != plato[id].Precio){
+      let val = plato[id].Precio;
+      let total = plato[id].Total;
+      let result = plato[id].Total - plato[id].Precio;
+      list[id].Total = result;
+      localStorage.setItem("pedidos", JSON.stringify(list));
+      obj++
+      setObj(obj);
+    } else {
+      list.splice(id, 1);
+      localStorage.setItem("pedidos", JSON.stringify(list));
+      obj++
+      setObj(obj);
+    }
+   
+  }
+
+  useEffect(()=>{
+   if(list.length){
+     let val = list.reduce((a, b) => a + b.Total,0);
+     document.querySelector(".total-car").textContent=Intl.NumberFormat().format(val); 
+   } else {
+     document.querySelector(".total-car").textContent=0;
+     document.querySelector("#btnActualizar").click();
+   }
+  });
+
   return(
     <div className="col-sm-8 menus-car" id="lista_menus">
-        <Modal isVisible={modal} setVisible={()=>setModal(false)}>
+       { modal &&
+         <Modal isVisible={modal} setVisible={()=>setModal(false)}>
           <div className="styleModal">
             <h3 className="text-center">
               Eliminar menú
             </h3>
             <h5 className="mt-2 text-center">
-              ¿Desea eliminar el menú {number && number} del carrito?
+              ¿Desea eliminar el menú {list.length && list[number].Nombre} del carrito?
             </h5>
             <div className="btns">
               <button className="closeModal">CANCELAR</button>
@@ -40,6 +87,9 @@ function All_menus (){
             </div>
           </div>
         </Modal>
+
+       }
+       <br id="btnActualizarMenu" style={{display:"none"}} onClick={()=>{obj++; setObj(obj)}}/>  
       {list.length ? 
         list.map((menu,index)=>{
           return (
@@ -50,9 +100,9 @@ function All_menus (){
               foto={menu['Foto']}
               cant ={eval(menu['Total'] + "/" + menu['Precio'])}
               id={index}
-              abr={(e)=>{setNumber(menu["Nombre"]); setModal(true)}}
-              del={del}
-              del1={()=>{setDel([0, false])}}
+              abr={()=>abrir(index)}
+              sum={(id)=>sumar(id)}
+              res={(id)=>restar(id)}
             />
           )
         }) :
