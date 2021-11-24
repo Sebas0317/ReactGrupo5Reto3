d471bdeaa6c0902a496b9197e7843e2619e6ba75
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2021-11-23 22:24:50 -0500

    View history added

--- src/Components/view-history/history.js
+++ src/Components/view-history/history.js
@@ -1,8 +1,41 @@
-export default function History (){
+import react, { useState } from "react";
+import MenuContainer from "../view-menu/MenuContainer";
+import "../styles/menu.css";
+function History() {
+  const listPlatos = JSON.parse(window.localStorage.getItem("history"));
+  return (
+    <>
+      <div className="row">
+        <div className="header-car pt-5 my-5">
+          <p className="title-car ps-5">Historial de compras</p>
+        </div>
+      </div>
 
-	return (
-		<div>
-			
-		</div>
-	)
+      <div className="platos">
+        {listPlatos.length ? (
+          listPlatos.map((plato) => {
+            return (
+              <>
+                <div className="item">
+                  <img src={plato.Foto} style={{width:'15em',height:'auto'}} alt="img-plato" />
+                  <div className="descripcion">
+                    <h1 className="titleItem">{plato.Nombre}</h1>
+                    <p></p>
+                    <h2 className="titlePrecio">
+                      {" "}
+                      $ {Intl.NumberFormat().format(plato.Precio)}
+                    </h2>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        ) : (
+          <h2 className="text">No hay historial disponibles</h2>
+        )}
+      </div>
+    </>
+  );
 }
+
+export default History;
